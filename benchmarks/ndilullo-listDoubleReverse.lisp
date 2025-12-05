bc559530bff3d42d2bdd8194c73e91854c416961
NicoDiLullo <ndilullo@thacher.org>
2025-12-05 18:05:59 -0500

    formatting and cse test

--- benchmarks/ndilullo-listDoubleReverse.lisp
+++ benchmarks/ndilullo-listDoubleReverse.lisp
@@ -1,13 +1,13 @@
 (define (reverse_helper lst acc)
-  (if (is-pair lst)
-      (reverse_helper (right lst) (pair (left lst) acc))
-      acc))
+    (if (is-pair lst)
+        (reverse_helper (right lst) (pair (left lst) acc))
+        acc))
 
 (define (reverse lst)
-  (reverse_helper lst nil))
+    (reverse_helper lst nil))
 
 
 (do
-  (let ((alist (pair 42 (pair 2 (pair 3 nil)))))
-    (let ((anotherlist (reverse (reverse alist))))
-      (print (left anotherlist)))))
+    (let ((alist (pair 42 (pair 2 (pair 3 nil)))))
+        (let ((anotherlist (reverse (reverse alist))))
+            (print (left anotherlist)))))
