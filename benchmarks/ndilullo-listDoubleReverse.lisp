55ce1c203a760e6e76d1ccd076579a59f53c8ba6
NicoDiLullo <ndilullo@thacher.org>
2025-12-05 18:49:01 -0500

    better version of double reverse

--- benchmarks/ndilullo-listDoubleReverse.lisp
+++ benchmarks/ndilullo-listDoubleReverse.lisp
@@ -1,13 +1,12 @@
 (define (reverse_helper lst acc)
-    (if (is-pair lst)
+    (if (pair? lst)
         (reverse_helper (right lst) (pair (left lst) acc))
         acc))
 
 (define (reverse lst)
-    (reverse_helper lst nil))
+    (reverse_helper lst ()))
 
 
 (do
-    (let ((alist (pair 42 (pair 2 (pair 3 nil)))))
-        (let ((anotherlist (reverse (reverse alist))))
-            (print (left anotherlist)))))
+    (let ((alist (pair 42 (pair 2 (pair 3 ())))))
+            (print (left (reverse (reverse alist))))))
