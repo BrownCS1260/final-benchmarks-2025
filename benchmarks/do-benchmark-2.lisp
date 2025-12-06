056f4a0d6a24d2e86954043bce500b80e0cc9bcf
Dawood Oosaala Olaniyi <114123801+DaOlaniyi@users.noreply.github.com>
2025-12-05 22:52:59 -0500

    Replace arithmetic with recursive sum function

--- benchmarks/do-benchmark-2.lisp
+++ benchmarks/do-benchmark-2.lisp
@@ -1,3 +1,7 @@
-(let ((x 10)
-      (y 4))
-  (print (+ x (- y 1))))
+(do
+  (define (sum xs)
+    (if (empty? xs)
+        0
+        (+ (left xs) (sum (right xs)))))
+  (print
+    (sum (pair 5 (pair 4 (pair 3 (pair 2 (pair 1 ()))))))))
