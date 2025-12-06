b742a954bb59c9f834cca517a8d13923e7ea44b9
Dawood Oosaala Olaniyi <114123801+DaOlaniyi@users.noreply.github.com>
2025-12-05 23:06:19 -0500

    3 files (dolaniyi)

---
+++ benchmarks/do-benchmark-1.lisp
@@ -0,0 +1,1 @@
+(print (left (right (pair 1 (pair 2 (pair 3 ()))))))
---
+++ benchmarks/do-benchmark-2.lisp
@@ -0,0 +1,1 @@
+(do (define (sum xs) (if (empty? xs) 0 (+ (left xs) (sum (right xs))))) (print (sum (pair 5 (pair 4 (pair 3 (pair 2 (pair 1 ()))))))))
