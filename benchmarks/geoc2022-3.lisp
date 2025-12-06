fb4b2d8ab30200a7d0864b199d54426eca7bddc7
Geoc2022 <geoc2022@gmail.com>
2025-12-05 23:28:46 -0500

    Fix base case

--- benchmarks/geoc2022-3.lisp
+++ benchmarks/geoc2022-3.lisp
@@ -1,6 +1,6 @@
 (define (multiply x y) (if (= y 0) 0 (+ x (multiply x (sub1 y)))))
 
-(define (expon x y) (if (= y 0) 0 (multiply x (expon x (sub1 y)))))
+(define (expon x y) (if (= y 0) 1 (multiply x (expon x (sub1 y)))))
 
 (let ((x 2))
   (let ((y 3))
