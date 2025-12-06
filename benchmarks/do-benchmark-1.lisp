0dffb33abe22d97db67ed3395d201ba0b5bf57c6
Dawood Oosaala Olaniyi <114123801+DaOlaniyi@users.noreply.github.com>
2025-12-05 22:50:57 -0500

    Refactor benchmark

--- benchmarks/do-benchmark-1.lisp
+++ benchmarks/do-benchmark-1.lisp
@@ -1,7 +1,4 @@
-(define (sum xs)
-  (if (empty? xs)
-      0
-      (+ (left xs) (sum (right xs)))))
-
 (print
-  (sum (pair 5 (pair 4 (pair 3 (pair 2 (pair 1 ())))))))
+  (left
+    (right
+      (pair 1 (pair 2 (pair 3 ()))))))
